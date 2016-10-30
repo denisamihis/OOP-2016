ff7c7be84ed17f8765a0e0e942f75b982cebb8be
Nicolas Cage <dumitrascu_dora@yahoo.com>
2016-10-30 16:52:13 +0200

    Update CaretakerFactory.java

--- Students/30221/Dumitrascu A. Dora/Zoo2/javasmmr/zoowsome/services/factories/CaretakerFactory.java
+++ Students/30221/Dumitrascu A. Dora/Zoo2/javasmmr/zoowsome/services/factories/CaretakerFactory.java
@@ -2,7 +2,7 @@ package javasmmr.zoowsome.services.factories;
 import javasmmr.zoowsome.models.employees.*;
 
 public class CaretakerFactory extends EmployeeAbstractFactory{
-	public Employee getEmployeeFactory(String type)throws Exception{ //?
+	public Employee getEmployeeFactory(String type)throws Exception{ //I get an error here ? Not sure about the architecture of the whole employee factory
 		if(Constants.Employee.Caretaker.equals(type)){
 			return new Caretaker();
 		}
