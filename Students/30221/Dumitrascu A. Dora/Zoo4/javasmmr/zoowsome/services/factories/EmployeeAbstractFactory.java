b60a72c60326308f515256e7fa484505d406fdd4
Dora <dumitrascu_dora@yahoo.com>
2016-11-18 14:54:02 +0200

    s

--- Students/30221/Dumitrascu A. Dora/Zoo4/javasmmr/zoowsome/services/factories/EmployeeAbstractFactory.java
+++
@@ -1,6 +0,0 @@
-package javasmmr.zoowsome.services.factories;
-import javasmmr.zoowsome.models.employees.*;
-
-public abstract class EmployeeAbstractFactory {
-	public abstract Employee getEmployeeFactory(String type);
-}
